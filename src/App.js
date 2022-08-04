c1e38c21c28b3daa8bf403e80c6c28f2a39e96b9
aboo-anas <muhamumdam@gmail.com>
2022-08-04 17:27:15 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Layouts from './components/Layouts';
+import './App.scss';
+import {Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <h1>Portfolio Website</h1>
+  );
+}
+
+export default App;
---
+++ src/components/Layouts/index.js
@@ -0,0 +1,8 @@
+import './index.scss'
+
+const Layouts = () => {
+    return <>Hello</>
+
+}
+
+export default Layouts
