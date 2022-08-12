835cea6599a92d3196a77fc48e52db82cf7ce9a0
aboo-anas <muhamumdam@gmail.com>
2022-08-12 21:15:31 +0100

    Nav-bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import Layouts from './components/Layouts';
 import './App.scss';
-import {Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar } from './components/Navbar/Navbar';
 
 
 function App() {
   return (
-    <h1>Portfolio Website</h1>
+    <Navbar/>
   );
 }
 
